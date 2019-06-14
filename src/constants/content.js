6508b8d7c45dda7c9141490a10112d04e40f1798
Oleksandr Tkach <a.tkach@soft-industry.com>
2019-06-14 09:49:13 +0300

    Specialists middle

---
+++ src/constants/content.js
@@ -0,0 +1,28 @@
+import heart from 'img/specialists/heart.png';
+import message from 'img/specialists/massage.png';
+import nurses from 'img/specialists/nurses.png';
+import therapy from 'img/specialists/therapy.png';
+
+const description = '10,000 zarejestrowanych opiekunów czeka na bezpośredni kontakt z poszukującymi opieki';
+export const SPECIALISTS = [
+    {
+        src: heart,
+        title: 'Opiekunowie z doświadczeniem',
+        description,
+    },
+    {
+        src: message,
+        title: 'Masażyści',
+        description,
+    },
+    {
+        src: nurses,
+        title: 'Pielęgniarki i opiekunowie medyczni',
+        description,
+    },
+    {
+        src: therapy,
+        title: 'Fizjoterapeuci',
+        description,
+    },
+];
