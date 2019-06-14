d04509e4a0aa02af945735fd7144e8b9da3ecbfa
Oleksandr Tkach <a.tkach@soft-industry.com>
2019-06-14 16:02:44 +0300

    HowItWorks fix padding

--- src/components/screens/HowItWorks.js
+++ src/components/screens/HowItWorks.js
@@ -33,7 +33,7 @@ export default function HowItWorks() {
                 <div className="container h-100">
                     <div className="row h-100">
 
-                        <div className="col-lg-6 how-it-works__about h-100">
+                        <div className="col-lg-6 how-it-works__about ">
                             <h3>
                                 Jak Działamy <br/> dla <br/> Poszukujących Opieki
                             </h3>
