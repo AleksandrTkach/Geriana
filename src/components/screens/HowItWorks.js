0338bdfaba506cd4ee983fcb708ea521e1a080ac
Oleksandr Tkach <a.tkach@soft-industry.com>
2019-06-14 10:15:18 +0300

    HowItWorks middle

--- src/components/screens/HowItWorks.js
+++ src/components/screens/HowItWorks.js
@@ -1,10 +1,51 @@
 import React from 'react';
 
+import {ROAD_POINTS} from "constants/content";
+
 export default function HowItWorks() {
+    const points = [];
+
+    for (let [index, point] of ROAD_POINTS.entries()) {
+
+
+        points.push(
+            <div className="road-points">
+                <div className="road-points__item">
+
+                    <div className="road-points__icon">
+                        <img src={point.img.src} alt={point.img.alt}/>
+                    </div>
+
+                    <h2 className="road-points__title">{point.label}</h2>
+
+
+                    <p className="road-points__description">{point.text}</p>
+
+                </div>
+            </div>
+        )
+    }
+
     return (
-        <div className='screen'>
+        <section className='screen'>
             <div className='screen__header screen__header_bg-how-it-works'/>
-            <div className='screen__body screen__body_bg-how-it-works'/>
-        </div>
+
+            <div className='screen__body screen__body_bg-how-it-works how-it-works'>
+                <div className="container h-100">
+                    <div className="row h-100">
+
+                        <div className="col-lg-6 how-it-works__about h-100">
+                            <h3>
+                                Jak Działamy <br/> dla <br/> Poszukujących Opieki
+                            </h3>
+                            <button className="btn btn_lg btn_outline"> ZNAJDŹ OPIEKUNKĘ </button>
+                        </div>
+
+                        <div className="col-lg-6">{ points }</div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
--- src/constants/content.js
+++ src/constants/content.js
@@ -3,6 +3,11 @@ import message from 'img/specialists/massage.png';
 import nurses from 'img/specialists/nurses.png';
 import therapy from 'img/specialists/therapy.png';
 
+import magnifier from "img/how-it-works/magnifier.svg";
+import mouse from "img/how-it-works/mouse.svg";
+import hourglass from "img/how-it-works/hourglass.svg";
+import dollar from "img/how-it-works/dollar.svg";
+
 const description = '10,000 zarejestrowanych opiekunów czeka na bezpośredni kontakt z poszukującymi opieki';
 export const SPECIALISTS = [
     {
@@ -24,5 +29,40 @@ export const SPECIALISTS = [
         src: therapy,
         title: 'Fizjoterapeuci',
         description,
+    },
+];
+
+export const ROAD_POINTS =  [
+    {
+        img: {
+            src: magnifier,
+            alt: "magnifier",
+        },
+        label: "Search a Caregiver",
+        text: "Ustalasz kryteria wyboru i  wyszukujesz opiekuna z  wymaganymi",
+    },
+    {
+        img: {
+            src: mouse,
+            alt: "mouse",
+        },
+        label: "Select a Caregiver",
+        text: "Decydujesz na podstawie  weryfikacji profilu i opinii inyych  o",
+    },
+    {
+        img: {
+            src: hourglass,
+            alt: "hourglass",
+        },
+        label: "Test Period",
+        text: "Opiekun rozpoczyna okres próbny  i jeśli jesteście zadowoleni,",
+    },
+    {
+        img: {
+            src: dollar,
+            alt: "dollar",
+        },
+        label: "Pay after Start",
+        text: "Opiekun dostaje wynagrodzenie dopiero po rozpoczęciu pracy. Po",
     },
 ];
