bcadfc3fc0bee993a110c64ff5f8476d0f64ae0c
Oleksandr Tkach <a.tkach@soft-industry.com>
2019-06-14 09:01:37 +0300

    TopBanner middle

--- src/components/screens/TopBanner.js
+++ src/components/screens/TopBanner.js
@@ -2,6 +2,21 @@ import React from 'react';
 
 export default function TopBanner() {
     return (
-        <div className='screen screen_bg-top-banner'/>
+        <section className='screen screen_bg-top-banner top-banner'>
+
+            <h1 className="top-banner__label">
+                <span className="pick-out-text"> Znajdź opiekuna </span>
+                <div> dla swoich najbliższych </div>
+            </h1>
+
+            <p className="top-banner__description">
+                Skontaktuj się bezpośrednio z jedną z
+                <span className="pick-out-text"> 60,000 zarejestrowanych </span>
+                <br/> opieklinek
+            </p>
+
+            <button className="btn btn_lg btn_bg-red"> ZNAJDŹ OPIEKUNKĘ </button>
+
+        </section>
     )
 }
