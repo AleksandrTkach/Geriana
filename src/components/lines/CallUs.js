ed978b44c385665ed03d1733d5c338de257a1942
Oleksandr Tkach <a.tkach@soft-industry.com>
2019-06-14 16:48:10 +0300

    CallUs done

--- src/components/lines/CallUs.js
+++ src/components/lines/CallUs.js
@@ -3,10 +3,9 @@ import React from 'react';
 export default function CallUs() {
     return (
         <section className='call-us'>
-            <h3 className="call-us__label">
-                Chcesz wiedziedzieć więcej?
-            </h3>
-            <span> Odwiedź nasze centrum pomocy lub zadzwoń </span>
+            <h3 className="call-us__label"> Chcesz wiedziedzieć więcej? </h3>
+            <span  className="call-us__description"> Odwiedź nasze centrum pomocy lub zadzwoń </span>
+            <button className='btn btn_md btn_bg-red'> Call +48 567 365 485 </button>
         </section>
     )
 }
