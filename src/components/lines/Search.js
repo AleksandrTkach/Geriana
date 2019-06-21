010f4c45d678e8fd78476eb44de38b249b1de416
Oleksandr Tkach <a.tkach@soft-industry.com>
2019-06-21 15:47:53 +0300

    Lines fix БЭМ

--- src/components/lines/Search.js
+++ src/components/lines/Search.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Search() {
     return (
         <section className='search'>
-            Want to care with Geriana? &nbsp; <span className='pick-out-text pick-out-text_underline'> Become a Caregiver </span>
+            Want to care with Geriana? &nbsp;
+            <a href='/#become-a-caregiver' className='link search__link'> Become a Caregiver </a>
         </section>
     )
 }
