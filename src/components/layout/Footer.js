ca7f8dc9cb0bae0a66ddda998762903c7ed7d371
Oleksandr Tkach <a.tkach@soft-industry.com>
2019-06-21 16:46:52 +0300

    Media fix for new БЭМ

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -26,7 +26,7 @@ export default function Footer() {
             <div className='container'>
                 <div className='row'>
 
-                    <div className='col-lg-5 col-md-5 col-sm-5'>
+                    <div className='col-lg-5 col-md-5 col-sm-5 footer__column'>
                         <img src={logo} alt={REACT_APP_NAME} className='footer__logo'/>
                         <div className='footer__follow-us'>
                             Follow Us
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -15,29 +15,33 @@ export default function Header() {
         <header id='top-menu' className='header'>
 
             <nav className='h-100'>
-                <div className='container h-100 d-flex align-items-center justify-content-between top-menu'>
+                <div className='container h-100 top-menu'>
+                    <div className="row h-100">
+                        <div className="col-12 d-flex align-items-center justify-content-between">
 
-                    <div>
-                        <img src={logo} alt={REACT_APP_NAME} className='top-menu-logo'/>
+                            <div>
+                                <img src={logo} alt={REACT_APP_NAME} className='top-menu-logo'/>
 
-                        <button className='btn btn_size_md btn_outline top-menu-btn'>
-                            <i className='fa fa-question-circle top-menu-btn-icon-question-circle'/>
-                            Help center
-                        </button>
-                        <button className='btn btn_size_md btn_outline top-menu-btn'>
-                            Call us
-                        </button>
+                                <button className='btn btn_size_md btn_outline top-menu-btn'>
+                                    <i className='fa fa-question-circle top-menu-btn-icon-question-circle'/>
+                                    Help center
+                                </button>
+                                <button className='btn btn_size_md btn_outline top-menu-btn'>
+                                    Call us
+                                </button>
+                            </div>
+
+                            <div className='top-menu-block-links'>
+                                <a href='/#/sing-in' className='link top-menu-link'> Sing Up </a>
+                                <a href='/#/log-in' className='link top-menu-link'> Log in </a>
+                                <a href='/#/menu' className='link top-menu-link'>
+                                    <i className='fa fa-bars bars top-menu-link-icon-bars'/>
+                                    Menu
+                                </a>
+                            </div>
+
+                        </div>
                     </div>
-
-                    <div className='top-menu-block-links'>
-                        <a href='/#/sing-in' className='link top-menu-link'> Sing Up </a>
-                        <a href='/#/log-in' className='link top-menu-link'> Log in </a>
-                        <a href='/#/menu' className='link top-menu-link'>
-                            <i className='fa fa-bars bars top-menu-link-icon-bars'/>
-                            Menu
-                        </a>
-                    </div>
-
                 </div>
             </nav>
 
