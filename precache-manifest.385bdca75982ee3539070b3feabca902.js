89b4254b103a7e882ebaf4332d774eae61da8cd2
Oleksandr Tkach <a.tkach@soft-industry.com>
2019-07-01 12:38:17 +0300

    Updates

---
+++ precache-manifest.385bdca75982ee3539070b3feabca902.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5bbd7b2fd4c2f353e66138e7dc018c8",
+    "url": "/Geriana/index.html"
+  },
+  {
+    "revision": "f1e3b502f2f87e34c822",
+    "url": "/Geriana/static/css/main.3d26dd43.chunk.css"
+  },
+  {
+    "revision": "ab5ac00df64b04b6fce3",
+    "url": "/Geriana/static/js/2.de8a68c4.chunk.js"
+  },
+  {
+    "revision": "f1e3b502f2f87e34c822",
+    "url": "/Geriana/static/js/main.c12854f0.chunk.js"
+  },
+  {
+    "revision": "37197cb91b2134976c1d",
+    "url": "/Geriana/static/js/runtime~main.c594e038.js"
+  },
+  {
+    "revision": "a2c6877fa431a373a5fb3bbb7a6305c0",
+    "url": "/Geriana/static/media/arrow.a2c6877f.svg"
+  },
+  {
+    "revision": "57266ee00a9b97ae6c86f0de84f68c49",
+    "url": "/Geriana/static/media/bg-how-it-works.57266ee0.jpg"
+  },
+  {
+    "revision": "7312fc3f0695fe7ab2f6d98c2aa2f7fd",
+    "url": "/Geriana/static/media/bg-our-preferences-heart.7312fc3f.svg"
+  },
+  {
+    "revision": "9679d6559f287e73cac9f5f16846fc39",
+    "url": "/Geriana/static/media/bg-our-preferences.9679d655.jpg"
+  },
+  {
+    "revision": "973d842ed5ec426119b759eaeb5a01c8",
+    "url": "/Geriana/static/media/bg-recalls.973d842e.jpg"
+  },
+  {
+    "revision": "44e95a203b498194d1dda69692dcd74a",
+    "url": "/Geriana/static/media/bg-top-banner.44e95a20.jpg"
+  },
+  {
+    "revision": "cd63cea7636037f85694b22c0dc09c95",
+    "url": "/Geriana/static/media/dollar.cd63cea7.svg"
+  },
+  {
+    "revision": "f02b2fee06daaa3d6435c7539a5a834b",
+    "url": "/Geriana/static/media/fb-icon.f02b2fee.svg"
+  },
+  {
+    "revision": "f6f478e81dcae69edd569040e9eb05cc",
+    "url": "/Geriana/static/media/hourglass.f6f478e8.svg"
+  },
+  {
+    "revision": "be280b7b2d0095f7857bf2f516289af3",
+    "url": "/Geriana/static/media/in-icon.be280b7b.svg"
+  },
+  {
+    "revision": "48b4d95f7a4d67e8cbd93773dd54f6a4",
+    "url": "/Geriana/static/media/magnifier.48b4d95f.svg"
+  },
+  {
+    "revision": "4ff3171d9fe9517d7dbb490bd9ef777f",
+    "url": "/Geriana/static/media/mouse.4ff3171d.svg"
+  },
+  {
+    "revision": "440e631356e95577e246f15b37dd6a4c",
+    "url": "/Geriana/static/media/tw-icon.440e6313.svg"
+  }
+]);
